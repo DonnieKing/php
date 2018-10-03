b232f76042950887eadf93cb4ff62dcb5ff2e37a
DonnieKing <974309696@qq.com>
2018-10-03 18:59:02 +0800

    cookie练习

---
+++ arraytest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/1 0001
+ * Time: 15:40
+ */
---
+++ testCookie.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/3 0003
+ * Time: 17:37
+ */
+
+
+     setcookie("user","wangchu",time()+3600); //setcookie() 函数必须位于 <html> 标签之前
+
+     echo $_COOKIE['user']."<br>"; // Print a cookie
+     print_r($_COOKIE);             // A way to view all cookies
+     echo "<br>";
+
+    if(isset($_COOKIE))
+    {
+        echo "welcome ".$_COOKIE['user']."<br>";
+    }
+    else{
+        echo "welcome guest"."<br>";
+    }
+
+   // setcookie("user","",time()-3600);//当删除 cookie 时，您应当使过期日期变更为过去的时间点。
+?>
