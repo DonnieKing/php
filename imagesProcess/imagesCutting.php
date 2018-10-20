5af0ffe90d3d8c0d49b83820b57873ca4f29cc39
DonnieKing <974309696@qq.com>
2018-10-20 21:11:41 +0800

    图片裁剪

--- imagesProcess/imagesCutting.php
+++ imagesProcess/imagesCutting.php
@@ -8,15 +8,15 @@
 header('content-type:text/html;charset=utf-8;');
 error_reporting(E_ALL & ~E_NOTICE);  //表示提示除去 E_NOTICE 之外的所有错误信息
 //打开来源图片
-$image = imagecreatefrompng('haha.jpg');
+$image = imagecreatefrompng('mmm.png');
 
 
 //定义百分比，缩放到0.1大小
-$percent = 0.1;
+$percent = 0.5;
 
 
 // 将图片宽高获取到
-list($width, $height) = getimagesize('haha.jpg');
+list($width, $height) = getimagesize('mmm.png');
 
 //设置新的缩放的宽高
 $new_width = $width * $percent;
@@ -27,7 +27,7 @@ $new_image = imagecreatetruecolor($new_width, $new_height);
 
 //将原图$image按照指定的宽高，复制到$new_image指定的宽高大小中
 imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
- 
+
 header('content-type:image/jpeg');
 imagejpeg($new_image);
 ?>
