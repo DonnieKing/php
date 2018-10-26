2357d505efe0f84f0dc2cae5a0bc9d7d2d86b26e
DonnieKing <974309696@qq.com>
2018-10-26 22:07:47 +0800

    类与继承练习

---
+++ classANDextends/1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/10/26 0026
+ * Time: 15:27
+ */
+
+    class wangchu{
+
+       public $arr;
+}
+
+        $a = new wangchu();
+        $a->attr = "value";
+        echo $a->attr;
+
+?>
