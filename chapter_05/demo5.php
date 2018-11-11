da209aebdded4bd857fc0c1ee7aed811e5686e59
DonnieKing <974309696@qq.com>
2018-11-12 07:40:39 +0800

    pdo添加操作

--- chapter_05/demo5.php
+++ chapter_05/demo5.php
@@ -1,6 +1,7 @@
 <?php
 //pdo 添加操作
-
+//PDOStatement::rowCount — 返回受上一个 SQL 语句影响的行数
+//PDOStatement::errorInfo — 获取跟上一次语句句柄操作相关的扩展错误信息
 
 //1.创建PDO对象,连接数据库
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=php_edu;charset=utf8','root','123456');
@@ -10,10 +11,10 @@ $sql = "INSERT INTO `user` (`name`,`sex`,`age`,`email`,`password`,`status`,`crea
 $stmt = $pdo->prepare($sql);
 
 //3.参数绑定
-$name='东方不败';
-$sex = 0 ;
-$age = 38;
-$email='dfbb@php.cn';
+$name='灭绝师太';
+$sex = 1 ;
+$age = 46;
+$email='mjst@php.cn';
 $password = sha1('123456');
 $status = 1;
 $create_time = time();
@@ -29,7 +30,7 @@ $stmt->bindParam('create_time',$create_time,PDO::PARAM_STR,20);
 //3.执行添加
 if($stmt->execute())
 {
-    ($stmt->rowCount()>0)?'成功添加了'.$stmt->rowCount().'条记录':'没有记录';
+    echo ($stmt->rowCount()>0)?'成功添加了'.$stmt->rowCount().'条记录':'没有记录';
 }
 else{
     exit(print_r($stmt->errorInfo(),true));
