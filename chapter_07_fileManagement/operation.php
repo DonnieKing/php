f2c5c0064c25b3379819409539a7cf883983413a
DonnieKing <974309696@qq.com>
2018-11-19 22:22:51 +0800

    操作配置文件

--- chapter_07_fileManagement/operation.php
+++ chapter_07_fileManagement/operation.php
@@ -55,6 +55,7 @@ HERE;
 }
 else if($act == 'doEdit')
 {
+     // 获取修改内容
      $content = $_REQUEST['content'];
      $filename = $_REQUEST['filename'];
      var_dump($filename);
@@ -70,13 +71,15 @@ else if($act == 'doEdit')
     alertMes($mes,$url);
 }
 
+
 // 判断点击操作（重命名）
 else if($act=='renameFile')
 {
     $str = <<<HERE
 <form action="index.php?act=doRename" method="post">
 请输入要修改的名字：<input type="text" name="rename" placeholder="请输入文件名">
-<input type="hidden" name="filename" value="$filename">
+<input type="hidden" name="path" value="{$path}">
+<input type="hidden" name="filename" value="{$filename}">
 <input type="submit" value="提交">
 </form>
 HERE;
@@ -84,9 +87,86 @@ HERE;
 }
 else if($act == 'doRename')
 {
+    // 接收新名称
     $rename = $_REQUEST['rename'];
+    // 进行修改文件名
     $mes = rename_file($filename,$path."/".$rename);
     alertMes($mes,$url);
+}
 
 
+// 判断点击操作（复制）
+else if($act == 'copyFile')
+{
+    //制作表单
+    $str = <<<HERE
+<form action="index.php?act=doCopyFile" method="post">
+将文件复制到：<input type="text" name="destName" placeholder="将文件复制到">
+<input type="hidden" name="filename" value="{$filename}">
+<input type="hidden" name="path" value="{$path}">
+<input type="submit" value="提交">
+</form> 
+HERE;
+    echo $str;
+}
+else if($act =='doCopyFile' )
+{
+    // 接收目标目录
+    $destName = $_REQUEST['destName'];
+    // 进行复制操作
+    $mes = copy_file($filename,$path."/".$destName);
+    // 提示操作
+    alertMes($mes,$url);
+}
+
+
+// 判断点击操作（剪切）
+else if($act == 'cutFile')
+{
+    $str = <<<HERE
+<form action="index.php?act=doCutFile" method="post">
+文件剪切到：<input type="text" name="destName" placeholder="文件剪切到：">
+<input type="hidden" name="path" value="{$path}">
+<input type="hidden" name="filename" value="{$filename}">
+<input type="submit" value="提交">
+</form>
+HERE;
+    echo $str;
+}
+else if($act == 'doCutFile')
+{
+    //表单中有自己手动输入的值，需要使用$_request来获取
+    $destName = $_REQUEST['destName'];
+    $mes = cut_file($filename,$path."/".$destName);
+    alertMes($mes,$url);
+}
+
+
+// 判断点击操作（下载）
+else if($act == 'dowFile')
+{
+    $mes = dow_file($filename);
+    alertMes($mes,$url);
+}
+
+
+// 判断点击操作（删除）
+else if($act == 'delFile')
+{
+    if(unlink($filename))
+    {
+        alertMes("文件删除成功",$url);
+    }
+    else
+    {
+        alertMes("文件删除失败",$url);
+    }
+}
+
+
+else if($act == "上传文件")
+{
+    $fileInfo = $_FILES['myFile'];
+    $mes = upload_file($fileInfo,$path);
+    alertMes($mes,$url);
 }
