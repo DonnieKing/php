53c76be074e6ece7c42f57581a34dcb27b5fae7f
DonnieKing <974309696@qq.com>
2018-11-19 08:45:34 +0800

    公共提示信息

--- chapter_07_fileManagement/common.php
+++ chapter_07_fileManagement/common.php
@@ -2,7 +2,9 @@
 /**
  * 公共提示信息
  */
+
 function alertMes($mes,$url)
 {
-    echo "<script>alert('{$mes}'); location.href='{$url}'</script>";
+    //创建成功后不能直接刷新页面，会一直提交表单进入死循环状态
+    echo "<script>alert('{$mes}');location.href='{$url}'</script>";
 }
