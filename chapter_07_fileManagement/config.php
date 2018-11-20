21b1fc801c3ac11f0086ac62c7601f6d5d0ea30c
DonnieKing <974309696@qq.com>
2018-11-20 12:03:28 +0800

    配置文件

--- chapter_07_fileManagement/config.php
+++ chapter_07_fileManagement/config.php
@@ -2,13 +2,14 @@
 /**
  * 配置文件
  */
-
+//引入方法类库
 include "function.php";
-
+//设置主目录
 $path = 'file';
-
+//读取主目录内容
+@$path = $_REQUEST['path']?$_REQUEST['path']:$path;
 $data = read_dir($path);
-
+//判断目录中是否有内容
 if(!$data)
 {
     echo "<script>alert('无文件或目录')</script>";
@@ -19,5 +20,7 @@ if(!$data)
 @$act = $_REQUEST['act'];
 //接受创建的文件名
 @$filename = $_REQUEST['filename'];
+//接受创建的文件夹
+@$dirname = $_REQUEST['dirname'];
 
 @$url = "index.php?path={$path}";
